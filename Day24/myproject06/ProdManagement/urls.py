169463bf7b67b30ecc739ab055658583593792c1
Kiran <colkiran@gmail.com>
2022-11-01 14:03:50 +0530

    Merge remote-tracking branch 'origin/master'

--- Day24/myproject06/ProdManagement/urls.py
+++ Day24/myproject06/ProdManagement/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path("", views.add_show, name="add"),
+    path("updatedata/<int:id>", views.update_data, name="updatedata"),
+    path("deletedata/<int:id>", views.delete_data, name="deletedata")
 ]
--- Day24/myproject06/ProdManagement/views.py
+++ Day24/myproject06/ProdManagement/views.py
@@ -1,3 +1,41 @@
+
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from .forms import ProductDetails
+from .models import Product
 
 # Create your views here.
+def add_show(request):
+    if request.method == 'POST':
+        fm = ProductDetails(request.POST)
+        if fm.is_valid():
+            pid = fm.cleaned_data['Prodid']
+            pnm = fm.cleaned_data['Prodname']
+            cat = fm.cleaned_data['Category']
+            pty = fm.cleaned_data['Prodtype']
+            prc = fm.cleaned_data['Price']
+            reg = Product(Prodid=pid, Prodname=pnm, Category=cat, Prodtype=pty, Price=prc)
+            reg.save()
+            fm = ProductDetails()
+    else:
+        fm = ProductDetails()
+    stud = Product.objects.all()
+    return render(request, 'add.html', {'form': fm, 'stu':stud})
+
+def update_data(request, id):
+    if request.method == 'POST':
+        pi = Product.objects.get(pk=id)
+        fm = ProductDetails(request.POST, instance=pi)
+        if fm.is_valid():
+            fm.save()
+            return HttpResponseRedirect("/")
+    else:
+        pi = Product.objects.get(pk=id)
+        fm = ProductDetails(instance=pi)
+    return render(request, "update.html", {'form': fm})
+
+def delete_data(request, id):
+    if request.method == 'POST':
+        pi = Product.objects.get(pk=id)
+        pi.delete()
+        return HttpResponseRedirect("/")
